a1f6c0c60798cf70456affbefc99667b219b570c
Hugo Pasquier <79862478+HugoPasquier@users.noreply.github.com>
2022-11-21 19:55:02 +0100

    Update mnist_nn.py
    Adding some comment and deleting useless portions of code

--- mnist_nn.py
+++ mnist_nn.py
@@ -23,33 +23,7 @@ x_test = x_test / 255
 y_train = keras.utils.to_categorical(y_train)
 y_test = keras.utils.to_categorical(y_test)
 
-def exo1() :
-    # Exercice 1
-    # Q1
-    model = Sequential()    # définition du model (séquentiel = suite de couche)
-    model.add(Flatten(input_shape = (28, 28)))  # réduction des image 28x28 en un vecteur (couche entrée)
-    # Ajout d'une couche caché pour Q3
-    #model.add(Dense(1, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(2, input_dim = 784, kernel_initializer='normal', activation='softmax')) # couche de traitement à 2 neurones, loi normal pour poids de depart
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])   # optimisation : descencte en gradient
-
-    model.summary()
-
-    # Q2
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=100)
-    scores = model.evaluate(X_test, y_test)
-    print("Neural network accuracy: %.2f%%" % (scores[1]*100))
-
-    # Pour améliorer la précisison du réseau sans changer son architecture, on peut faire varier 
-    # le nombre d'epochs et le batch_size
-
-    # Q3 
-    # L'ajout de la couche caché améliore légérement l'efficacité du réseau
-    # Pour Q2, batch_size = 10 -> 97,49%, batch_size = 100 -> 96.74%
-    # Pour Q3, batch_size = 10 -> 97,60%, batch_size = 100 -> 96.86%
-
-
-# Exercice 2
+## Build Classic Model without convulation
 def classic_model() :
 
     model = Sequential()    # Create model
@@ -72,6 +46,7 @@ def classic_model() :
     #batch_size = 100 ~ 92.80%
 
 
+## Build a Convolution Neurone Network
 def CNN_model() :
 
     model = Sequential()
@@ -88,8 +63,9 @@ def CNN_model() :
     model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=50) 
     scores = model.evaluate(x_test, y_test)
     print("Neural network accuracy: %.2f%%" % (scores[1]*100))
-    #batch_size = 100 -> 99.14%
+    #batch_size = 100 ~ 99.14%
 
 ################ Main ################
 
-classic_model()
+#classic_model()
+CNN_model()
